346fd1fdfd56752f6039df89a4307595f9a216d1
Earle F. Philhower, III <earlephilhower@yahoo.com>
2018-02-14 12:55:13 -0800

    Use time() to get time by default
    configTime() on the ESP8266 lets you use NTP to set the internal
    system clock. This means the UNIX time() function can be used instead
    of requiring the minimal_set_time call.

--- src/inner.h
+++ src/inner.h
@@ -413,7 +413,7 @@
  */
 
 #ifndef BR_USE_UNIX_TIME
-#if defined __unix__ || defined __linux__ \
+#if defined __unix__ || defined __linux__ || defined ESP8266 \
 	|| defined _POSIX_SOURCE || defined _POSIX_C_SOURCE \
 	|| (defined __APPLE__ && defined __MACH__)
 #define BR_USE_UNIX_TIME   1
