c6ffcd29381f555783a75d957c28c0ff861cd33a
Thomas Pornin <pornin@bolet.org>
2018-09-12 17:42:08 +0200

    Fixed warning on GCC 4.6 to 4.9 (macro redefinition).

--- src/inner.h
+++ src/inner.h
@@ -2469,8 +2469,8 @@ int br_ssl_choose_hash(unsigned bf);
 #else
 #define BR_TARGETS_X86_UP \
 	_Pragma("GCC target(\"sse2,ssse3,sse4.1,aes,pclmul\")")
-#endif
 #define BR_TARGETS_X86_DOWN
+#endif
 #pragma GCC diagnostic ignored "-Wpsabi"
 #endif
 
