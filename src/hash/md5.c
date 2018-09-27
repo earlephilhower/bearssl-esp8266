e2c37d03643caec94f0eb01423dd657b244c043d
Earle F. Philhower, III <earlephilhower@yahoo.com>
2018-09-27 15:55:53 -0700

    Move MD5 constant to PROGMEM, save 48 bytes RAM

--- src/hash/md5.c
+++ src/hash/md5.c
@@ -59,7 +59,7 @@ static const uint32_t K[64] PROGMEM = {
 	0xF7537E82, 0xBD3AF235, 0x2AD7D2BB, 0xEB86D391
 };
 
-static const unsigned char MP[48] = {
+static const unsigned char MP[48] PROGMEM = {
 	1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,
 	5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
 	0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9
@@ -72,6 +72,8 @@ br_md5_round(const unsigned char *buf, uint32_t *val)
 	uint32_t m[16];
 	uint32_t a, b, c, d;
 	int i;
+	unsigned char mem_MP[48];
+	memcpy_P(mem_MP, MP, 48);
 
 	a = val[0];
 	b = val[1];
@@ -91,22 +93,22 @@ br_md5_round(const unsigned char *buf, uint32_t *val)
 		b = c + ROTL(b + F(c, d, a) + m[i + 3] + K[i + 3], 22);
 	}
 	for (i = 16; i < 32; i += 4) {
-		a = b + ROTL(a + G(b, c, d) + m[MP[i - 16]] + K[i + 0],  5);
-		d = a + ROTL(d + G(a, b, c) + m[MP[i - 15]] + K[i + 1],  9);
-		c = d + ROTL(c + G(d, a, b) + m[MP[i - 14]] + K[i + 2], 14);
-		b = c + ROTL(b + G(c, d, a) + m[MP[i - 13]] + K[i + 3], 20);
+		a = b + ROTL(a + G(b, c, d) + m[mem_MP[i - 16]] + K[i + 0],  5);
+		d = a + ROTL(d + G(a, b, c) + m[mem_MP[i - 15]] + K[i + 1],  9);
+		c = d + ROTL(c + G(d, a, b) + m[mem_MP[i - 14]] + K[i + 2], 14);
+		b = c + ROTL(b + G(c, d, a) + m[mem_MP[i - 13]] + K[i + 3], 20);
 	}
 	for (i = 32; i < 48; i += 4) {
-		a = b + ROTL(a + H(b, c, d) + m[MP[i - 16]] + K[i + 0],  4);
-		d = a + ROTL(d + H(a, b, c) + m[MP[i - 15]] + K[i + 1], 11);
-		c = d + ROTL(c + H(d, a, b) + m[MP[i - 14]] + K[i + 2], 16);
-		b = c + ROTL(b + H(c, d, a) + m[MP[i - 13]] + K[i + 3], 23);
+		a = b + ROTL(a + H(b, c, d) + m[mem_MP[i - 16]] + K[i + 0],  4);
+		d = a + ROTL(d + H(a, b, c) + m[mem_MP[i - 15]] + K[i + 1], 11);
+		c = d + ROTL(c + H(d, a, b) + m[mem_MP[i - 14]] + K[i + 2], 16);
+		b = c + ROTL(b + H(c, d, a) + m[mem_MP[i - 13]] + K[i + 3], 23);
 	}
 	for (i = 48; i < 64; i += 4) {
-		a = b + ROTL(a + I(b, c, d) + m[MP[i - 16]] + K[i + 0],  6);
-		d = a + ROTL(d + I(a, b, c) + m[MP[i - 15]] + K[i + 1], 10);
-		c = d + ROTL(c + I(d, a, b) + m[MP[i - 14]] + K[i + 2], 15);
-		b = c + ROTL(b + I(c, d, a) + m[MP[i - 13]] + K[i + 3], 21);
+		a = b + ROTL(a + I(b, c, d) + m[mem_MP[i - 16]] + K[i + 0],  6);
+		d = a + ROTL(d + I(a, b, c) + m[mem_MP[i - 15]] + K[i + 1], 10);
+		c = d + ROTL(c + I(d, a, b) + m[mem_MP[i - 14]] + K[i + 2], 15);
+		b = c + ROTL(b + I(c, d, a) + m[mem_MP[i - 13]] + K[i + 3], 21);
 	}
 
 	val[0] += a;
